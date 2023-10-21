6a79967797eca4fac81f36064f3d5db3ef011182
Afmer <afmerlord@gmail.com>
2023-10-21 17:10:03 +0300

    Add html download for component

--- Frontend/src/Components/Home.tsx
+++ Frontend/src/Components/Home.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+interface PartViewData {
+    html: string;
+  }
 function Home() {
+    const [htmlObj, setHtmlObj] = useState<PartViewData | null>(null);
+    useEffect(() => {
+        fetch('/api/Home')  // Путь начинается с '/'
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data : PartViewData) => {
+            setHtmlObj(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }, []);
     return (
         <div>
-            <h1>hello</h1>
-            <p>Театральный клуб "В Созвездиях" приглашает вас в удивительный мир искусства под открытым небом. Мы расположены в самом центре города и предлагаем незабвенные впечатления от разнообразных представлений. Наши актеры, режиссеры и художники творят с любовью к искусству, создавая удивительные постановки - от классики до современности. Помимо театральных представлений, мы организовываем мастер-классы, встречи с актерами и лекции по искусству. Наш театр расположен в уютном парке, где зрители могут наслаждаться природой и искусством в едином гармоничном образе. Присоединяйтесь к нам и погрузитесь в удивительный мир театра "В Созвездиях". Наша команда с нетерпением ждет встречи с вами и обещает незабвенные впечатления!</p>
+            <div dangerouslySetInnerHTML={{ __html: htmlObj ? htmlObj.html : "" }} />
         </div>
     )
 }
