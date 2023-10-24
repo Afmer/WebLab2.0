3317045d70e7f383b3da8092085398dda61e3915
Afmer <afmerlord@gmail.com>
2023-10-24 16:15:41 +0300

    Add extra parameters for jwt config

--- Backend/Architecture/Configurations/JwtConfiguration.cs
+++ Backend/Architecture/Configurations/JwtConfiguration.cs
@@ -4,4 +4,6 @@ public class JwtConfiguration
 {
     public string Key {get; set;} = null!;
     public string Algorithm {get; set;} = null!;
+    public string Issuer {get; set;} = null!;
+    public string Audience {get; set;} = null!;
 }
