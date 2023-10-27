0e0510ff26d96bad97d01032a577481a69560e95
Afmer <grevich13@gmail.com>
2023-10-27 10:59:47 +0300

    Init ImageController

---
+++ Backend/Controllers/ImageController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Weblab.Architecture.Interfaces;
+
+namespace Weblab.Controllers;
+
+public class ImageController : ControllerBase
+{
+    private readonly IImageService _imageService;
+    public ImageController(IImageService imageService)
+    {
+        _imageService = imageService;
+    }
+    [HttpGet]
+    public IActionResult Show(Guid id, string imageArea)
+    {
+        try
+        {
+            var path = _imageService.GetImagePath(imageArea, id);
+            FileStream fileStream = new(path, FileMode.Open, FileAccess.Read);
+            return File(fileStream, "image/jpeg");
+        }
+        catch
+        {
+            return NotFound();
+        }
+    }
+}
